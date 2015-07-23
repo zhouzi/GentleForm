34fcf59d123f3626ff37075a8019c9d35577e327
Gabin <hello@gabinaureche.com>
2015-07-23 21:15:03 +0200

    remove doc page

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,6 @@ var gulp          = require('gulp');
 var $             = require('gulp-load-plugins')();
 var webpackConfig = require('./webpack.config.js');
 var pkg           = require('./package.json');
-var browserSync   = require('browser-sync');
-var reload        = browserSync.reload;
-var generate      = require('./generate.js');
 
 var header = [
     '/*!',
@@ -19,21 +16,6 @@ var header = [
     ''
 ].join('\n');
 
-gulp.task('serve', ['default'], function () {
-    browserSync({ notify: false, port: 9000, server: { baseDir: ['.'] } });
-    gulp.watch(['index.html', 'styles.css']).on('change', reload);
-    gulp.run('watch');
-});
-
-gulp.task('styles', function () {
-    return gulp
-        .src('*.scss')
-        .pipe($.sass({ outputStyle: 'compressed' }))
-        .pipe($.autoprefixer('last 2 version'))
-        .pipe(gulp.dest(''))
-    ;
-});
-
 gulp.task('scripts', function () {
     return gulp
         .src('src/entry.js')
@@ -44,12 +26,6 @@ gulp.task('scripts', function () {
 
 gulp.task('watch', ['default'], function () {
     gulp.watch('src/*.js', ['scripts']);
-    gulp.watch('*.scss', ['styles']);
-    gulp.watch('README.md', ['doc']);
 });
 
-gulp.task('doc', function (done) {
-    generate(done);
-});
-
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['scripts']);
