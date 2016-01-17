7dea739d109ffb9b9048d03a92ef2fdafab388ac
Gabin <hello@gabinaureche.com>
2016-01-17 17:40:24 +0100

    clean and simplify workflow

--- src/utils.js
+++
@@ -1,5 +0,0 @@
-export default {
-    isButton ($element) {
-        return $element.prop('tagName') == 'button' || ['button', 'submit'].indexOf($element.attr('type')) > -1;
-    }
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
+var path = require('path')
+var argv = require('yargs').argv
+
+var pkg = require('./package.json')
+var banner = [
+  '/*!',
+  ` * ${pkg.name} - v${pkg.version}`,
+  ` * ${pkg.description}`,
+  ` * ${pkg.homepage}`,
+  ' *',
+  ` * @author ${pkg.author}`,
+  ` * @license ${pkg.license}`,
+  ' */',
+  ''
+].join('\n')
 
 module.exports = {
-    entry: './src/GentleForm.js',
-
-    output: {
-        path: __dirname + '/dist',
-        filename: 'GentleForm.min.js',
-        library: 'GentleForm',
-        libraryTarget: 'umd'
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel-loader']
-            }
-        ]
-    },
-
-    plugins: [new webpack.optimize.UglifyJsPlugin()]
-};
+  entry: './src/GentleForm.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'GentleForm' + (argv.p ? '.min' : '') + '.js',
+    library: 'GentleForm',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin(banner, { raw: true })
+  ]
+}
